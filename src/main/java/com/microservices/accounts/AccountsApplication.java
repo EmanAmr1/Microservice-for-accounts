b51c0fb7347b96fc1783267cd877957da80d75b2
EmanAmr1 <emanamr543@gmail.com>
2024-10-15 17:14:56 +0300

    Enhancing documentation of REST APIs using @OpenAPIDefinition

--- src/main/java/com/microservices/accounts/AccountsApplication.java
+++ src/main/java/com/microservices/accounts/AccountsApplication.java
@@ -1,11 +1,28 @@
 package com.microservices.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(title = "Accounts microservice REST API Documentation",
+		             description = "Bank Accounts microservice REST API Documentation",
+				     version = "v1",
+				     contact = @Contact(
+							 name = "eman",
+							 email = "eman@gmail.com"
+					 ),
+				     license = @License(
+							 name = "Apache 2.0"
+		             ))
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
