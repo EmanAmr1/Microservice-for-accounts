29a809def21980b6fe13f0addaaa6b0a1a58d6ba
EmanAmr1 <emanamr543@gmail.com>
2024-10-21 11:43:51 +0300

    Exceptions

---
+++ cards/src/main/java/com/microservices/cards/Exception/GlobalExceptionHandle.java
@@ -0,0 +1,13 @@
+package com.microservices.cards.Exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandle extends ResponseEntityExceptionHandler {
+
+//    public ResponseEntity<Object> GlobalExceptionHandle(){
+//
+//    }
+}
